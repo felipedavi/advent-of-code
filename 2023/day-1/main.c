7f25c66e4932a35d9b900570c4a00b3e5bb8327d
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2023-12-10 08:40:19 -0300

    simplifying my solution from part 1 - day 1 - AoC 2023

--- 2023/day-1/main.c
+++ 2023/day-1/main.c
@@ -5,34 +5,32 @@
 
 int main(void) {
     FILE *fp;
-    char line[1024];
+    char ch, start = 0, end = 0;
     int sum = 0;
-
+    
     fp = fopen("input.txt", "r");
 
-    while (fgets(line, sizeof(line), fp) != NULL) {
-        line[strcspn(line, "\n")] = '\0';
-
-        char *start = NULL;
-        char *end = NULL;
-
-        for (int i = 0; line[i] != '\0'; i++) {
-            if (isdigit(line[i])) {
-                if (start == NULL) {
-                    start = &line[i];
-                }
-                end = &line[i];
+    while(1) {
+        ch = fgetc(fp);
+        if(feof(fp)) 
+            break;
+        if(ch != '\n') {
+            if(isdigit(ch)) {
+                if (start == '\0') 
+                    start = ch;
+                end=ch;
             }
+        } else {
+            char n[] = {start, end, '\0'};            
+            sum+=atoi(n);
+            start = end = '\0';
         }
-
-        if (start != NULL && end != NULL) {
-            char digit[3] = {start[0], end[0], '\0'};
-            sum += atoi(digit);
-        }
+        continue;
     }
 
     fclose(fp);
 
-    printf("Part 1: %d\n", sum);
+    printf("Sum of calibration values: %d\n", sum);
+
     return 0;
 }
